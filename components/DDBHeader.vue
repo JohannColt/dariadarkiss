a8628911ab27fc2d1008cd7d1b33a99f10440df4
Dante de Braso <sergey.king96@gmail.com>
2020-07-11 11:58:33 +0300

    Init commit

---
+++ components/DDBHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <header class="ddb-header">
+
+  </header>
+</template>
+
+<script>
+    export default {
+        name: "ddb-header"
+    }
+</script>
+
+<style scoped>
+  .ddb-header {
+    padding: 0 40px;
+  }
+</style>
