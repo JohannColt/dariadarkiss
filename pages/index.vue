faced6e9718c4ab1c603aa2bac2901a70bffdb78
serge <sergey.king96@gmail.com>
2020-08-28 13:50:28 +0300

    Fixes scrolling on the main page

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,11 @@
 <template>
-  <section class="main-container">
-    <div :class="mainClasses" ref="main">
-      <ddb-main-slider/>
-      <ddb-second-block/>
-      <ddb-slider/>
+  <section class="vertical-magnet">
+    <div class="magnet-scroll" ref="main">
+      <ddb-main-slider class="slide slide-1"/>
+      <div class="slide slide-2 scrollable">
+        <ddb-second-block/>
+        <ddb-slider/>
+      </div>
     </div>
   </section>
 </template>
@@ -14,63 +16,130 @@ import DDBMainSlider from "../components/DDBMainSlider";
 import DDBSlider from "@/components/DDBLastWorks";
 
 export default {
-  data() {
-    return {
-      maxItems: 1,
-      currentItem: 0,
-      scrolling: false,
-      mainClasses: 'main-page main-page--hidden-overflow'
-    }
-  },
   components: {
     'ddb-second-block': DDBSecondBlock,
     'ddb-main-slider': DDBMainSlider,
     'ddb-slider': DDBSlider
   },
+  data() {
+    return {
+      magneticScrollableSlides: false,
+      verticalMagnet: false,
+      magnetScroll: false,
+      slides: [],
+      lastScroll: false,
+      threshold: 700,
+      slideHeight: 80,
+      slideHeights: [100, 100],
+      touchThreshold: 100,
+      currentSlide: 1,
+      exitingScrollable: false,
+      touchStart: false,
+      topOnTouchStart: false,
+      parsed: 0
+    }
+  },
   computed: {
+    numberOfSlides() {
+      return this.slides.length
+    }
   },
   methods: {
-    onWheel(e) {
-      const delta = e.deltaY || e.detail || e.wheelDelta;
-      if (delta > 0) {
-        if (this.currentItem === this.maxItems) {
-          return
+    getAccumulatedHeight() {
+      let acc = 0;
+      this.slideHeights.forEach((it, ind) => {
+        if (this.currentSlide - 1 > ind) {
+          acc += it;
         }
-        this.mainClasses = 'main-page'
-        this.currentItem ++;
-        const y = this.currentItem * 100;
-        this.$refs.main.style.transform = 'translateY(' + -y + 'vh)';
-      } else {
-        if (this.currentItem === 0) {
-          return
+      });
+      return acc
+    },
+    getPercentageOfScroll(element) {
+      return (
+        (element.scrollTop / (element.scrollHeight - element.clientHeight)) * 100
+      );
+    },
+    getTime() {
+      return new Date().getTime();
+    },
+    initScrolling() {
+      this.magneticScrollableSlides = document.querySelectorAll(".scrollable");
+      this.verticalMagnet = document.querySelector(".vertical-magnet");
+      this.magnetScroll = document.querySelector(".magnet-scroll");
+      this.slides = document.querySelectorAll(".slide");
+      this.lastScroll = this.getTime();
+      [...this.slides].forEach((i, index) => {
+        i.style.height = this.slideHeights[index] + 'vh';
+      });
+      this.verticalMagnet.addEventListener("touchstart", (e) => {
+        this.touchStart = e.touches[0].clientY;
+        this.parsed = 1;
+      });
+      this.verticalMagnet.addEventListener("touchmove", (e) => {
+        if (this.parsed === 1) {
+          const touchMove = e.touches[0].clientY;
+          if (Math.abs(touchMove - this.touchStart) > this.touchThreshold) {
+            if (touchMove - this.touchStart > 0) {
+              this.currentSlide -= 1;
+            } else {
+              this.currentSlide += 1;
+            }
+            this.magnetScroll.style.top = this.getAccumulatedHeight() * -1 + "vh";
+            this.parsed = 2;
+          }
         }
-        if (window.scrollY < 100) {
-          this.mainClasses = 'main-page main-page--hidden-overflow'
-          this.currentItem --;
-          const y = this.currentItem * 100;
-          this.$refs.main.style.transform = 'translateY(' + -y + 'vh)';
+      });
+      this.verticalMagnet.addEventListener("touchend", () => {
+        this.parsed = 0;
+      });
+      this.verticalMagnet.addEventListener("wheel", (e) => {
+        const current = document.querySelector(".slide-" + this.currentSlide);
+        if (current.className.indexOf("scrollable") === -1 || this.exitingScrollable) {
+          const time = this.getTime();
+          if (this.lastScroll + this.threshold < time) {
+            this.lastScroll = time;
+            if (e.deltaY > 0) {
+              if (this.currentSlide !== this.numberOfSlides) {
+                this.currentSlide += 1;
+              }
+            } else {
+              if (this.currentSlide !== 1) {
+                this.currentSlide -= 1;
+              } else {
+                this.lastScroll -= this.threshold;
+              }
+            }
+            this.magnetScroll.style.top = this.getAccumulatedHeight() * -1 + "vh";
+            this.exitingScrollable = false;
+          }
         }
-      }
+      });
+      [...this.magneticScrollableSlides].forEach((el) => {
+        el.addEventListener("wheel", (ev) => {
+          const elementSlide = parseInt(
+            el.className.match(/slide\-[0-9]+/)[0].replace(/slide\-([0-9]+)/, "$1")
+          );
+          const scroll = this.getPercentageOfScroll(el);
+          if (
+            elementSlide === this.currentSlide &&
+            ((scroll === 0 && ev.deltaY < 0) || (scroll === 100 && ev.deltaY > 0))
+          ) {
+            this.exitingScrollable = true;
+          }
+        });
+      });
     }
   },
   mounted() {
-    if (window.innerWidth < 1200) {
-      return;
-    }
-    if ('onwheel' in document) {
-      window.addEventListener("wheel", this.onWheel);
-    } else if ('onmousewheel' in document) {
-      window.addEventListener("mousewheel", this.onWheel);
-    } else {
-      // Firefox < 17
-      window.addEventListener("MozMousePixelScroll", this.onWheel);
+    if (window.innerWidth > 992) {
+      this.initScrolling();
     }
   }
 }
 </script>
 
 <style lang="scss">
-.main-container {
+.vertical-magnet {
   height: 100vh;
   height: calc(var(--vh, 1vh) * 100);
   width: 100%;
@@ -79,37 +148,27 @@ export default {
   align-items: center;
   text-align: center;
 }
-.main-page {
-  height: 100%;
+
+.magnet-scroll {
+  position: absolute;
+  transition: top 750ms ease;
+  top: 0;
+}
+.slide {
+  height: 100vh;
+  height: calc(var(--vh, 1vh) * 100);
 }
 @include for-desktop-up {
-  .main-page {
-    width: 100%;
-    transition: 1s;
+  .vertical-magnet {
+    width: 100vw;
+    overflow: hidden;
+    position: relative;
   }
-  .main-page--up {
-    transform: translateY(100vh);
-  }
-  .main-page--down {
-    transform: translateY(-100vh);
-  }
-  .main-container {
-    &--hidden-overflow {
-      overflow: hidden;
-    }
-  }
-}
 
-.page-item-enter {
-  transform: translateY(100vh);
-}
+  .slide.scrollable {
+    overflow-y: scroll;
+  }
 
-.page-item-enter-active {
-  transition: all 0.5s ease;
-}
-
-.page-item-leave {
-  transform: translateY(-100vh);
 }
 
 </style>
