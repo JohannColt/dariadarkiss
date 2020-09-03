d4282f123eb1afa987bd0a2863956fb14cba851d
ivan <vanesk97@gmail.com>
2020-09-03 19:42:21 +0300

    *Desktop Popular-Photo-Session block

--- components/DDBPopularPhotosession.vue
+++ components/DDBPopularPhotosession.vue
@@ -2,68 +2,88 @@
   <div class="ddb-popular">
     <div class="ddb-popular__container container">
       <div class="ddb-popular__heading">
-        <h3 class="ddb-popular__heading__text">
+        <h2 class="ddb-popular__heading__text ddb-popular__heading__text__mobile">
           Популярные<br/> фотосессии
-        </h3>
+        </h2>
+        <h2 class="ddb-popular__heading__text ddb-popular__heading__text__desktop">
+          Популярные фотосессии
+        </h2>
       </div>
       <div class="ddb-popular__blocks">
-        <div class="ddb-popular__element">
-          <div class="ddb-popular__photo">
-            <img src="images/ui-elements/pop-photo.png">
+        <div class="ddb-popular__double">
+          <div class="ddb-popular__element ddb-popular__element__left ddb-popular__element__upper">
+            <div class="ddb-popular__photo ddb-popular__photo__mobile">
+              <img class="ddb-popular__image" src="images/ui-elements/big-smile.jpg">
+            </div>
+            <div class="ddb-popular__photo ddb-popular__photo__desktop">
+              <img src="images/ui-elements/pop-photo-h.png">
+            </div>
+            <div class="ddb-popular__description">
+              <p class="ddb-popular__description__ps">Фотосессия</p>
+              <h3 class="ddb-popular__description__name">Love Story</h3>
+              <p class="ddb-popular__description__des">Отличный способ для влюбленной пары провести время вместе,
+                сохранив
+                воспоминания. Съёмка может проходить как в студии, так и на улице.</p>
+              <div class="ddb-popular__description__button">
+                <p class="ddb-popular__description__more">Подробнее</p>
+                <img class="ddb-popular__description__line" src="images/ui-elements/button-line.png">
+              </div>
+            </div>
           </div>
-          <div class="ddb-popular__description">
-            <p class="ddb-popular__description__ps">Фотосессия</p>
-            <p class="ddb-popular__description__name">Love Story</p>
-            <p class="ddb-popular__description__des">Отличный способ для влюбленной пары провести время вместе, сохранив
-              воспоминания. Съёмка может проходить как в студии, так и на улице.</p>
-            <div class="ddb-popular__description__button">
-              <p class="ddb-popular__description__more">Подробнее</p>
-              <img class="ddb-popular__description__line" src="images/ui-elements/button-line.png">
+          <div class="ddb-popular__element ddb-popular__element__right ddb-popular__element__upper">
+            <div class="ddb-popular__photo ddb-popular__photo__mobile">
+              <img class="ddb-popular__image" src="images/ui-elements/big-smile.jpg">
+            </div>
+            <div class="ddb-popular__photo ddb-popular__photo__desktop">
+              <img src="images/ui-elements/pop-photo-h1.png">
+            </div>
+            <div class="ddb-popular__description">
+              <p class="ddb-popular__description__ps">Фотосессия</p>
+              <h3 class="ddb-popular__description__name">Fashion</h3>
+              <p class="ddb-popular__description__des">Идеальное решение для личного профайла или сайта. Это хороший
+                способ для девушек и мужчин, подчеркнуть свою индивидуальность.</p>
+              <div class="ddb-popular__description__button">
+                <p class="ddb-popular__description__more">Подробнее</p>
+                <img class="ddb-popular__description__line" src="images/ui-elements/button-line.png">
+              </div>
             </div>
           </div>
         </div>
-        <div class="ddb-popular__element">
-          <div class="ddb-popular__photo">
-            <img src="images/ui-elements/pop-photo1.png">
-          </div>
-          <div class="ddb-popular__description">
-            <p class="ddb-popular__description__ps">Фотосессия</p>
-            <p class="ddb-popular__description__name">Fashion</p>
-            <p class="ddb-popular__description__des">Идеальное решение для личного профайла или сайта. Это хороший
-              способ для девушек и мужчин, подчеркнуть свою индивидуальность.</p>
-            <div class="ddb-popular__description__button">
-              <p class="ddb-popular__description__more">Подробнее</p>
-              <img class="ddb-popular__description__line" src="images/ui-elements/button-line.png">
+        <div class="ddb-popular__double">
+          <div class="ddb-popular__element ddb-popular__element__left ddb-popular__element__lower">
+            <div class="ddb-popular__photo ddb-popular__photo__mobile">
+              <img class="ddb-popular__image" src="images/ui-elements/big-smile.jpg">
+            </div>
+            <div class="ddb-popular__photo ddb-popular__photo__desktop">
+              <img src="images/ui-elements/pop-photo-h2.png">
+            </div>
+            <div class="ddb-popular__description">
+              <p class="ddb-popular__description__ps">Фотосессия</p>
+              <h3 class="ddb-popular__description__name">Семейная</h3>
+              <p class="ddb-popular__description__des"> Идеальное решение для семьи сохранить светлые воспоминания в
+                виде фотографий, провести время вместе и оставить яркие моменты в памяти.</p>
+              <div class="ddb-popular__description__button">
+                <p class="ddb-popular__description__more">Подробнее</p>
+                <img class="ddb-popular__description__line" src="images/ui-elements/button-line.png">
+              </div>
             </div>
           </div>
-        </div>
-        <div class="ddb-popular__element">
-          <div class="ddb-popular__photo">
-            <img src="images/ui-elements/pop-photo2.png">
-          </div>
-          <div class="ddb-popular__description">
-            <p class="ddb-popular__description__ps">Фотосессия</p>
-            <p class="ddb-popular__description__name">Семейная</p>
-            <p class="ddb-popular__description__des">Отличный способ для влюбленной пары провести время вместе, сохранив
-              воспоминания. Съёмка может проходить как в студии, так и на улице.</p>
-            <div class="ddb-popular__description__button">
-              <p class="ddb-popular__description__more">Подробнее</p>
-              <img class="ddb-popular__description__line" src="images/ui-elements/button-line.png">
+          <div class="ddb-popular__element ddb-popular__element__right ddb-popular__element__lower">
+            <div class="ddb-popular__photo ddb-popular__photo__mobile">
+              <img class="ddb-popular__image" src="images/ui-elements/big-smile.jpg">
             </div>
-          </div>
-        </div>
-        <div class="ddb-popular__element">
-          <div class="ddb-popular__photo">
-            <img src="images/ui-elements/pop-photo3.png">
-          </div>
-          <div class="ddb-popular__description">
-            <p class="ddb-popular__description__ps">Фотосессия</p>
-            <p class="ddb-popular__description__name">Беременности</p>
-            <p class="ddb-popular__description__des">Отличный способ для беременной девушки сохранить память о самом
-              сильном и азартном ожидании, которое когда-либо приходилось испытывать.</p>
-            <div class="ddb-popular__description__button">
-              <p class="ddb-popular__description__more">Подробнее</p>
-              <img class="ddb-popular__description__line" src="images/ui-elements/button-line.png">
+            <div class="ddb-popular__photo ddb-popular__photo__desktop">
+              <img src="images/ui-elements/pop-photo-h3.png">
+            </div>
+            <div class="ddb-popular__description">
+              <p class="ddb-popular__description__ps">Фотосессия</p>
+              <h3 class="ddb-popular__description__name">Беременности</h3>
+              <p class="ddb-popular__description__des">Отличный способ для беременной девушки сохранить память о самом
+                сильном и азартном ожидании, которое когда-либо приходилось испытывать.</p>
+              <div class="ddb-popular__description__button">
+                <p class="ddb-popular__description__more">Подробнее</p>
+                <img class="ddb-popular__description__line" src="images/ui-elements/button-line.png">
+              </div>
             </div>
           </div>
         </div>
@@ -80,6 +100,9 @@
 
 <style lang="scss" scoped>
   .ddb-popular {
+    display: flex;
+    justify-content: center;
+
     &__container {
       display: flex;
       flex-direction: column;
@@ -91,11 +114,12 @@
       padding-left: 24px;
 
       &__text {
-        font-weight: bold;
-        font-size: 24px;
-        line-height: 32px;
         margin-top: 64px;
         margin-bottom: 24px;
+
+        &__desktop {
+          display: none;
+        }
       }
     }
 
@@ -105,14 +129,29 @@
     }
 
     &__element {
+      display: flex;
+      flex-direction: column;
       margin-top: 16px;
       margin-bottom: 0;
     }
 
     &__photo {
-      object-fit: none;
-      height: 160px;
-      width: 360px;
+      &__mobile {
+        height: 160px;
+        position: relative;
+        display: flex;
+      }
+
+      &__desktop {
+        display: none;
+      }
+    }
+
+    &__image {
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
     }
 
     &__description {
@@ -169,6 +208,121 @@
         left: 0;
         width: 102px;
         height: 1px;
+      }
+    }
+  }
+
+  @include for-extra-large {
+    .ddb-popular {
+      &__container {
+
+      }
+
+      &__heading {
+        padding: 0;
+
+        &__text {
+          &__mobile {
+            display: none;
+          }
+
+          &__desktop {
+            display: block;
+          }
+        }
+      }
+
+      &__blocks {
+
+      }
+
+      &__double {
+        display: flex;
+        flex-direction: row;
+      }
+
+      &__element {
+        &__left {
+          margin-right: 8px;
+        }
+
+        &__right {
+          margin-left: 8px;
+        }
+
+        &__upper {
+          margin-bottom: 8px;
+        }
+
+        &__lower {
+          margin-top: 8px;
+        }
+
+        max-width: 622px;
+        max-height: 350px;
+        display: flex;
+        flex-direction: row;
+      }
+
+      &__photo {
+        &__mobile {
+          display: none;
+        }
+
+        &__desktop {
+          display: flex;
+        }
+      }
+
+      &__description {
+        &__ps {
+          margin-top: 32px;
+          font-weight: normal;
+          font-size: 16px;
+          line-height: 32px;
+        }
+
+        &_name {
+
+        }
+
+        &__des {
+          font-weight: normal;
+          font-size: 16px;
+          line-height: 32px;
+        }
+
+        &__button {
+
+        }
+
+        &__more {
+          margin-bottom: 54px;
+        }
+
+        &__line {
+          bottom: 54px;
+        }
+      }
+
+    }
+  }
+  @include for-medium{
+    .ddb-popular{
+      &__photo{
+        &__mobile{
+          height: 320px;
+        }
+      }
+    }
+  }
+  @include for-large{
+
+    .ddb-popular{
+      &__photo{
+        &__mobile{
+          height: 380px;
+        }
       }
     }
   }
